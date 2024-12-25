96cd3e3b605fec1df33e416842d58134f96ea241
HM-RunningHub <haimacloud001@gmail.com>
2024-12-26 02:01:18 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,22 @@
 from .RH_SettingsNode import SettingsNode
 from .RH_NodeInfoListNode import NodeInfoListNode
 from .RH_ExecuteNode import ExecuteNode
+from .RH_ImageUploaderNode import ImageUploaderNode
+
+
 
 NODE_CLASS_MAPPINGS = {
     "RH_SettingsNode": SettingsNode,
     "RH_NodeInfoListNode": NodeInfoListNode,
     "RH_ExecuteNode": ExecuteNode,
+    "RH_ImageUploaderNode": ImageUploaderNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "RH_SettingsNode": "RH Settings",
     "RH_NodeInfoListNode": "RH Node Info List",
     "RH_ExecuteNode": "RH Execute",
+    "RH_ImageUploaderNode": "RH Image Uploader",
 }
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS",]
